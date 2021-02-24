c804f8382df6116f9342919ece9458e6f918aa40
Rebkova <rebkova@gmail.com>
2021-02-24 15:34:53 +0100

    diamonds made and clickable

---
+++ app.js
@@ -0,0 +1,20 @@
+const diamondBlue = document.getElementById("race-track__button-1")
+const diamondGreen = document.getElementById("race-track__button-2")
+const diamondRed = document.getElementById("race-track__button-3")
+const diamondYellow = document.getElementById("race-track__button-4")
+
+diamondBlue.onclick = () => {
+  console.log("Hej, you clicked the blue button!")
+}
+
+diamondGreen.onclick = () => {
+  console.log("Hej, you clicked the green button!")
+}
+
+diamondRed.onclick = () => {
+  console.log("Hej, you clicked the red button!")
+}
+
+diamondYellow.onclick = () => {
+  console.log("Hej, you clicked the yellow button!")
+}
