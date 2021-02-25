701fd0a7902c1a404b0297bb1105b2a01265eb07
Rebkova <rebkova@gmail.com>
2021-02-25 17:27:17 +0100

    counter and moving working, but collectively

--- app.js
+++ app.js
@@ -3,19 +3,77 @@ const greenDiamond = document.getElementById("greenButton")
 const redDiamond = document.getElementById("redButton")
 const yellowDiamond = document.getElementById("yellowButton")
 
+const blueCounter = document.getElementById("blueScore")
+const greenCounter = document.getElementById("greenScore")
+const redCounter = document.getElementById("redScore")
+const yellowCounter = document.getElementById("yellowScore")
+console.log(blueCounter)
+
+let n = 0
+const startPosition = -17
+const endPosition = 583
+const moveToRight = (endPosition - startPosition) / 10
+console.log(`initial int: ${n}`)
+let currentPosition = startPosition
+console.log(`Curr pos: ${currentPosition}`)
+
 blueDiamond.onclick = () => {
   console.log("Hej, you clicked the blue button!")
-  //
+
+  if (currentPosition < endPosition) {
+    console.log(`End position: ${endPosition}`)
+    n += 1
+    console.log(`Int: ${n}`)
+    currentPosition = startPosition + moveToRight * n
+    console.log(`Curr pos: ${currentPosition}`)
+    blueDiamond.style.marginLeft = currentPosition + "px"
+    blueCounter.innerHTML = n
+  } else if (currentPosition === endPosition) {
+    console.log(`Hej, I'm here`)
+  }
 }
 
 greenDiamond.onclick = () => {
   console.log("Hej, you clicked the green button!")
+  if (currentPosition < endPosition) {
+    console.log(`End position: ${endPosition}`)
+    n += 1
+    console.log(`Int: ${n}`)
+    currentPosition = startPosition + moveToRight * n
+    console.log(`Curr pos: ${currentPosition}`)
+    greenDiamond.style.marginLeft = currentPosition + "px"
+    greenCounter.innerHTML = n
+  } else if (currentPosition === endPosition) {
+    console.log(`Hej, I'm here`)
+  }
 }
 
 redDiamond.onclick = () => {
   console.log("Hej, you clicked the red button!")
+  if (currentPosition < endPosition) {
+    console.log(`End position: ${endPosition}`)
+    n += 1
+    console.log(`Int: ${n}`)
+    currentPosition = startPosition + moveToRight * n
+    console.log(`Curr pos: ${currentPosition}`)
+    redDiamond.style.marginLeft = currentPosition + "px"
+    redCounter.innerHTML = n
+  } else if (currentPosition === endPosition) {
+    console.log(`Hej, I'm here`)
+  }
 }
 
 yellowDiamond.onclick = () => {
   console.log("Hej, you clicked the yellow button!")
+  if (currentPosition < endPosition) {
+    console.log(`End position: ${endPosition}`)
+    n += 1
+    console.log(`Int: ${n}`)
+    currentPosition = startPosition + moveToRight * n
+    console.log(`Curr pos: ${currentPosition}`)
+    yellowDiamond.style.marginLeft = currentPosition + "px"
+    yellowCounter.innerHTML = n
+  } else if (currentPosition === endPosition) {
+    console.log(`Hej, I'm here`)
+  }
 }
